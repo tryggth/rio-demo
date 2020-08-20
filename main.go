4a372570fb5637b40aa848cee5027b91641881cb
tryggth <jimw@bugopolis.com>
2020-08-20 16:30:39 -0700

    Another test
    another test

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running a new verion")
+	fmt.Fprintln(w, "Hi there, I'm running a new verion v0.3")
 }
 
 func main() {
