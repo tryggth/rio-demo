3b829f8edb1e27b635e3eda0b3c6bc24d3b96dce
tryggth <jimw@bugopolis.com>
2020-08-21 07:09:40 -0700

    Updated to 0.6
    Updated to 0.6

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running a new verion v0.5")
+	fmt.Fprintln(w, "Hi there, I'm running a new verion v0.6")
 }
 
 func main() {
